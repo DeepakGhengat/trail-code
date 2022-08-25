a9454cc6afb4bb7d2509ae3c623ad485566c712d
chiragbadhe <badhechirag633@gmail.com>
2022-08-26 00:16:09 +0530

    fix: Design changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -52,7 +52,7 @@ function Hero() {
             </div>
             <div>
               <Fade duration={2000}>
-                <div className="max-w-[588px] sm:my-[75px] sm:my-0 pt-[49px] sm:pt-0 ">
+                <div className="max-w-[588px] sm:py-[75px] pt-[49px] sm:pt-[75px]">
                   <video
                     className="sm:h-[403px] sm:w-[588px]"
                     src="/images/heroVideo.mp4"
