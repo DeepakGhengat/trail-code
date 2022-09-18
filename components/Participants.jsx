2a3dcb53fdd92fc4b168e8c1ffc33d63ef17532f
chiragbadhe <badhechirag633@gmail.com>
2022-09-18 16:54:48 +0530

    ft:Content

--- components/Participants.jsx
+++ components/Participants.jsx
@@ -8,6 +8,8 @@ import React from "react";
 import Image from "next/image";
 import Fade from "react-reveal/Fade";
 
+import rightarrow from "../public/images/rightarrow.svg"
+
 function Participants() {
   return (
     <Fade>
@@ -23,11 +25,11 @@ function Participants() {
         </div>
         <Fade duration={2000}>
           <div className="grid sm:grid-cols-3 grid-cols-1 gap-[25px]">
-            <div className="relative group">
+            <a href="https://docs.defiedge.io/DefiEdge%20Participants/Liquidity%20Provision%20on%20DeFiEdge" target="_blank" className="relative group" rel="noreferrer">
               <div className="absolute w-[335px] 	">
-                <div className="w-full sm:w-[384px] h-[290px] bg-gradient-to-tl to-transparent from-[#58C0FB] opacity-16 group-hover:opacity-30 duration-300 border border-[#3F4077]/30"></div>
+                <div className="w-full sm:w-[384px] h-[330px] bg-gradient-to-tl to-transparent from-[#58C0FB] opacity-16 group-hover:opacity-30 duration-300 border border-[#3F4077]/30"></div>
               </div>
-              <div className="p-[20px] sm:p-[32px] items-start flex flex-col  relative h-[290px]">
+              <div className="p-[20px] sm:p-[32px] items-start flex flex-col  h-[330px]  relative h-[10px]">
                 <div>
                   <Image
                     type="image"
@@ -41,20 +43,20 @@ function Participants() {
                 </span>
                 <p className="text-textgray text-[16px] mt-[12px]">
                 Create personalised strategies with the ultimate features from the platform and manage your own liquidity.                </p>
-                {/* <span className="absolute bottom-[32px]  flex group cursor-pointer">
+                <div className="absolute bottom-[32px]  flex group cursor-pointer">
                   <p className="">Read More &nbsp;</p>{" "}
                   <span className=" group-hover:translate-x-2 duration-300 items-center flex">
                     <Image type="image" className="" alt="" src={rightarrow} />{" "}
                   </span>
-                </span> */}
+                </div>
               </div>
-            </div>
+            </a>
 
-            <div className="relative group">
+            <a href="https://docs.defiedge.io/DefiEdge%20Participants/Yeild%20Hunters" target="_blank" className="relative group" rel="noreferrer">
               <div className="absolute w-[335px] 	">
-                <div className="w-full sm:w-[384px] h-[290px] bg-gradient-to-tl to-transparent from-[#FABB18] opacity-16 group-hover:opacity-30 duration-300 border border-[#3F4077]/30"></div>
+                <div className="w-full sm:w-[384px] h-[330px] bg-gradient-to-tl to-transparent from-[#FABB18] opacity-16 group-hover:opacity-30 duration-300 border border-[#3F4077]/30"></div>
               </div>
-              <div className="p-[20px] sm:p-[32px] items-start flex flex-col  relative h-[290px]">
+              <div className="p-[20px] sm:p-[32px] items-start flex flex-col  relative h-[330px]">
                 <div>
                   <Image
                     type="image"
@@ -70,20 +72,20 @@ function Participants() {
                 Invest smartly in the best strategies managed by some of the best in the industry. Just sit back, relax and watch your funds grow.
                 </p>
 
-                {/* <span className="absolute bottom-[32px]  flex group cursor-pointer">
+                <a href="" target="_blank" className="absolute bottom-[32px]  flex group cursor-pointer">
                   <p className="">Read More &nbsp;</p>{" "}
                   <span className=" group-hover:translate-x-2 duration-300 items-center flex">
                     <Image type="image" className="" alt="" src={rightarrow} />{" "}
                   </span>
-                </span> */}
+                </a>
               </div>
-            </div>
+            </a>
 
-            <div className="relative group">
+            <a href="https://docs.defiedge.io/DefiEdge%20Participants/Managing%20Strategy%20on%20DefiEdge" target="_blank" className="relative group" rel="noreferrer">
               <div className="absolute w-[335px] 	">
-                <div className="w-full sm:w-[384px] h-[290px] bg-gradient-to-tl to-transparent from-[#45C646] opacity-16 group-hover:opacity-30 duration-300 border border-[#3F4077]/30"></div>
+                <div className="w-full sm:w-[384px] h-[330px] bg-gradient-to-tl to-transparent from-[#45C646] opacity-16 group-hover:opacity-30 duration-300 border border-[#3F4077]/30"></div>
               </div>
-              <div className="p-[20px] sm:p-[32px] items-start flex flex-col  relative h-[290px]">
+              <div className="p-[20px] sm:p-[32px] items-start flex flex-col  relative h-[330px]">
                 <div>
                   <Image type="image" alt="" src={p3} />
                 </div>
@@ -94,14 +96,14 @@ function Participants() {
                 Supercharge the returns on the assets with the cutting edge features and start earning performance fees managing the assets.
                 </p>
 
-                {/* <span className="absolute bottom-[32px]  flex group cursor-pointer">
+                <a href="" target="_blank" className="absolute bottom-[32px]  flex group cursor-pointer">
                   <p className="">Read More &nbsp;</p>{" "}
                   <span className=" group-hover:translate-x-2 duration-300 items-center flex">
                     <Image type="image" className="" alt="" src={rightarrow} />{" "}
                   </span>
-                </span> */}
+                </a>
               </div>
-            </div>
+            </a>
           </div>
         </Fade>
       </section>
