6351a9fb276a258de50fa48393e65bc8baec70c8
Pranav Raut <pranavraut033@gmail.com>
2022-11-20 13:49:12 +0530

    feat: ecosystem page

---
+++ data/auditors.js
@@ -0,0 +1,39 @@
+import damian from "../public/images/strategy/damian.png";
+import mudit from "../public/images/strategy/mudit.png";
+import riley from "../public/images/strategy/riley.png";
+import watchpug from "../public/images/strategy/watchpug.jpeg";
+import abdk from "../public/images/strategy/abdk.svg";
+import lucash from "../public/images/strategy/lucash.png";
+
+export default [
+  {
+    name: "Damian Rusinek",
+    logo: damian,
+    desc: "Securing",
+  },
+  {
+    name: "Mudit Gupta",
+    logo: mudit,
+    desc: "CISO, Polygon",
+  },
+  {
+    name: "Riley Holterhus",
+    logo: riley,
+    desc: "Security Auditor",
+  },
+  {
+    name: "ABDK",
+    logo: abdk,
+    desc: "Auditing Firm",
+  },
+  {
+    name: "WatchPug",
+    logo: watchpug,
+    desc: "Auditing Firm",
+  },
+  {
+    name: "Lucash Dev",
+    logo: lucash,
+    desc: "Auditor",
+  },
+];
---
+++ data/mentors.js
@@ -0,0 +1,73 @@
+import amrit from "../public/images/strategy/amrit.svg";
+import gsr from "../public/images/strategy/gsr.svg";
+import juan from "../public/images/strategy/juan.jpeg";
+import kevin from "../public/images/strategy/kevin.svg";
+import keyrock from "../public/images/strategy/keyrock.svg";
+import meme from "../public/images/strategy/meme.svg";
+import sancap from "../public/images/strategy/sancap.svg";
+import wintermute from "../public/images/strategy/wintermute.svg";
+import yuriy from "../public/images/strategy/yuriy.jpeg";
+import ziliqa from "../public/images/strategy/ziliqa.svg";
+
+export default [
+  {
+    id: 1,
+    name: "Wintermute",
+    logo: wintermute,
+    desc: "Market Maker",
+  },
+  {
+    id: 2,
+    name: "GSR",
+    logo: gsr,
+    desc: "Market Maker",
+  },
+  {
+    id: 8,
+    name: "Keyrock",
+    logo: keyrock,
+    desc: "Market Maker",
+  },
+  {
+    id: 6,
+    name: "MGNR",
+    logo: meme,
+    desc: "Market Maker",
+  },
+  {
+    id: 3,
+    name: "Sanctum Capital",
+    logo: sancap,
+    desc: "Investment Firm",
+  },
+  {
+    id: 4,
+    name: "Zilliqa Fund",
+    logo: ziliqa,
+    desc: "Creator Fund",
+  },
+  {
+    id: 5,
+    name: "Yuriy Myronovych",
+    logo: yuriy,
+    desc: "Quant & HFT Architect",
+  },
+  {
+    id: 7,
+    name: "Juan David",
+    logo: juan,
+    desc: "Founder, Keyrock",
+  },
+  {
+    id: 10,
+    name: "Kevin Beardsley",
+    logo: kevin,
+    desc: "Head MM, Kraken",
+  },
+  {
+    id: 11,
+    name: "Amrit Kumar",
+    logo: amrit,
+    desc: "Founder, Zilliqa",
+  },
+];
