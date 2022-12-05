b291bb6cd21e6fb6b29ab79b8f4598b7beab3f4a
Pranav Raut <pranavraut033@gmail.com>
2022-12-05 13:50:01 +0530

    feat: updated ecosystem page

--- data/ecosystemPartners.js
+++ data/ecosystemPartners.js
@@ -1,59 +1,49 @@
+import celo from "../public/images/ecosystem-partners/celo.png";
+import civilization from "../public/images/ecosystem-partners/civilization.png";
+import defibasket from "../public/images/ecosystem-partners/defibasket.png";
+import defilab from "../public/images/ecosystem-partners/defilab.png";
 import five from "../public/images/strategy/005.svg";
 import four from "../public/images/strategy/004.svg";
+import gelato from "../public/images/ecosystem-partners/gelato.png";
+import genki from "../public/images/ecosystem-partners/genki.png";
+import morpho from "../public/images/ecosystem-partners/morpho.jpg";
 import multifarm from "../public/images/strategy/multifarm.svg";
 import one from "../public/images/strategy/001.svg";
+import prep from "../public/images/ecosystem-partners/prep.svg";
+import quicknode from "../public/images/ecosystem-partners/quicknode.png";
 import six from "../public/images/strategy/006.svg";
 import solv from "../public/images/strategy/solv.svg";
+import stakedao from "../public/images/ecosystem-partners/stakedao.png";
+import swdoa from "../public/images/ecosystem-partners/swdoa.jpg";
 import three from "../public/images/strategy/003.svg";
 import two from "../public/images/strategy/002.svg";
+import zapper from "../public/images/ecosystem-partners/zapper.png";
 
 export default [
+  { name: "Ethereum", desc: "L1 Chain", logo: five },
+  { name: "Polygon", desc: "EVM Chain", logo: three },
+  { name: "Arbitrum", desc: "L2 Chain", logo: four },
+  { name: "Optimism", desc: "L2 Chain", logo: one },
+  { name: "1inch", desc: "DEX Aggregator", logo: two },
+  { name: "Chainlink", desc: "Price feed", logo: six },
+  { name: "Gelato", desc: "Automation", logo: gelato },
+  { name: "StakeDao", desc: "Yields and Backers", logo: stakedao },
+  { name: "Quicknode", desc: "RPC", logo: quicknode },
   {
-    id: 5,
-    name: "Ethereum",
-    logo: five,
-    desc: "Blockchain",
+    name: "Defilab",
+    desc: "Backtesting",
+    logo: defilab,
+    background: "#dfe9e7",
+    rounded: true,
   },
-  {
-    id: 3,
-    name: "Polygon",
-    logo: three,
-    desc: "ETH L2 Blockchain",
-  },
-  {
-    id: 4,
-    name: "Arbitrum",
-    logo: four,
-    desc: "ETH L2 Blockchain",
-  },
-  {
-    id: 1,
-    name: "Optimism",
-    logo: one,
-    desc: "Rollup Network",
-  },
-  {
-    id: 2,
-    name: "1 inch Protocol",
-    logo: two,
-    desc: "DEX Aggregator",
-  },
-  {
-    id: 6,
-    name: "Chainlink",
-    logo: six,
-    desc: "Decentralized Oracle ",
-  },
-  {
-    id: 7,
-    name: "Solv Protocol",
-    logo: solv,
-    desc: "NFT Trading",
-  },
-  {
-    id: 8,
-    name: "Multifarm",
-    logo: multifarm,
-    desc: "Data Platform",
-  },
+  { name: "Solv", desc: "Bonds", logo: solv },
+  { name: "SW DAO", desc: " Strategy", logo: swdoa },
+  { name: "Celo", desc: "EVM Chain", logo: celo },
+  { name: "Morpho", desc: "Lending protocol", logo: morpho },
+  { name: "Multifarm", desc: "Yield Aggregator", logo: multifarm },
+  { name: "Civilization", desc: "Fund management", logo: civilization },
+  { name: "Zapper", desc: "Yield Aggregator", logo: zapper },
+  { name: "Perpetual", desc: "Perpetual futures", logo: prep },
+  { name: "Genki Web", desc: " 3.0", logo: genki },
+  { name: "DefiBasket", desc: "Yield Generating", logo: defibasket },
 ];
--- pages/ecosystem.jsx
+++ pages/ecosystem.jsx
@@ -1,104 +1,78 @@
 import Image from "next/image";
-import React, { useEffect, useMemo, useReducer, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Circle from "../public/images/bg-ecosystem.svg";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 import allPartners from "../data/ecosystemPartners.js";
-import allAuditors from "../data/auditors.js";
-import allMentors from "../data/mentors.js";
+// import allAuditors from "../data/auditors.js";
+// import allMentors from "../data/mentors.js";
 import { Fade } from "react-reveal";
 
-LoadMoreButton.propTypes = {
-  active: PropTypes.bool.isRequired,
-};
+// LoadMoreButton.propTypes = {
+//   active: PropTypes.bool.isRequired,
+// };
 
-export function LoadMoreButton({ active, ...props }) {
-  return (
-    <button
-      {...props}
-      className="px-5 py-2 bg-default-blue/20 border border-default-blue rounded-full space-x-2 inline-flex items-center text-base leading-7"
-    >
-      <svg
-        className={active ? "rotate-180" : undefined}
-        width="25"
-        height="24"
-        viewBox="0 0 25 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M12.5 5V19"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M19.5 12L12.5 19L5.5 12"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+// export function LoadMoreButton({ active, ...props }) {
+//   return (
+//     <button
+//       {...props}
+//       className="px-5 py-2 bg-default-blue/20 border border-default-blue rounded-full space-x-2 inline-flex items-center text-base leading-7"
+//     >
+//       <svg
+//         className={active ? "rotate-180" : undefined}
+//         width="25"
+//         height="24"
+//         viewBox="0 0 25 24"
+//         fill="none"
+//         xmlns="http://www.w3.org/2000/svg"
+//       >
+//         <path
+//           d="M12.5 5V19"
+//           stroke="white"
+//           strokeWidth="2"
+//           strokeLinecap="round"
+//           strokeLinejoin="round"
+//         />
+//         <path
+//           d="M19.5 12L12.5 19L5.5 12"
+//           stroke="white"
+//           strokeWidth="2"
+//           strokeLinecap="round"
+//           strokeLinejoin="round"
+//         />
+//       </svg>
 
-      <span> {active ? "Show less" : "Load more"}</span>
-    </button>
-  );
-}
+//       <span> {active ? "Show less" : "Load more"}</span>
+//     </button>
+//   );
+// }
 
-const INITIAL_ITEMS_TO_SHOW = 8;
+// const INITIAL_ITEMS_TO_SHOW = 8;
 
 export default function EcoSystemPage() {
   const input = useRef(null);
 
-  const [
-    { showAllPartners, showAllAuditors, showAllMentors, searchInput },
-    dispatch,
-  ] = useReducer((state, newState) => ({ ...state, ...newState }), {
-    showAllPartners: false,
-    showAllAuditors: false,
-    showAllMentors: false,
-    searchInput: "",
-  });
+  // const [{ /* showAllPartners, */ searchInput }, dispatch] = useReducer(
+  //   (state, newState) => ({ ...state, ...newState }),
+  //   {
+  //     // showAllPartners: true,
+  //     searchInput: "",
+  //   }
+  // );
 
-  const searchText = searchInput.trim().toLowerCase();
+  // const searchText = searchInput.trim().toLowerCase();
 
-  const partners = useMemo(
-    () =>
-      allPartners
-        .slice(0, showAllPartners ? undefined : INITIAL_ITEMS_TO_SHOW)
-        .filter(
-          (e) =>
-            e.name.toLowerCase().includes(searchText) ||
-            e.desc.toLowerCase().includes(searchText)
-        ),
-    [showAllPartners, searchInput]
-  );
-
-  const auditors = useMemo(
-    () =>
-      allAuditors
-        .slice(0, showAllAuditors ? undefined : INITIAL_ITEMS_TO_SHOW)
-        .filter(
-          (e) =>
-            e.name.toLowerCase().includes(searchText) ||
-            e.desc.toLowerCase().includes(searchText)
-        ),
-    [showAllAuditors, searchInput]
-  );
-
-  const mentors = useMemo(
-    () =>
-      allMentors
-        .slice(0, showAllMentors ? undefined : INITIAL_ITEMS_TO_SHOW)
-        .filter(
-          (e) =>
-            e.name.toLowerCase().includes(searchText) ||
-            e.desc.toLowerCase().includes(searchText)
-        ),
-    [showAllMentors, searchInput]
-  );
+  // const partners = useMemo(
+  //   () =>
+  //     allPartners
+  //       // .slice(0, showAllPartners ? undefined : INITIAL_ITEMS_TO_SHOW)
+  //       .filter(
+  //         (e) =>
+  //           e.name.toLowerCase().includes(searchText) ||
+  //           e.desc.toLowerCase().includes(searchText)
+  //       ),
+  //   [/* showAllPartners, */ searchInput]
+  // );
 
   useEffect(() => {
     if (!input.current) return;
@@ -117,135 +91,85 @@ export default function EcoSystemPage() {
 
   return (
     <div className="relative w-full overflow-hidden">
-      <section className="border-b border-white/[0.08]">
+      {/* className="border-b border-white/[0.08]" */}
+      <section>
         <div className="text-center flex flex-col items-center">
           <h1 className="mt-20 mb-3 text-gradient text-[30px] leading-[41px] md:text-[44px] md:leading-[127%] font-semibold  max-w-[588px] ">
             Explore the DefiEdge Ecosystem
           </h1>
-          <p className="text-base leading-7 text-white/60 max-w-[500px] mb-8">
+          <p className="text-base leading-7 text-white/60 max-w-[500px] mb-10 md:mb-20">
             Swap, earn, vote, and more with hundreds of DeFi apps, integrations,
             and tools built on the Uniswap Protocol.
           </p>
-          <div className="focus-within:ring rounded px-5 py-[10px] border border-white/40 bg-[#d9d9d9]/10 mb-20">
+          {/* <div className="focus-within:ring rounded px-5 py-[10px] border border-white/40 bg-[#d9d9d9]/10 mb-20">
             <input
               ref={input}
               type="text"
               value={searchInput}
               onInput={(e) => {
-                const showButtons = !!e.target.value.trim();
+                // const showButtons = !!e.target.value.trim();
 
                 dispatch({
                   searchInput: e.target.value,
-                  showAllPartners: showButtons,
-                  showAllAuditors: showButtons,
-                  showAllMentors: showButtons,
+                  // showAllPartners: showButtons,
+                  // showAllAuditors: showButtons,
+                  // showAllMentors: showButtons,
                 });
               }}
               className="!outline-none bg-transparent text-base leading-6 placeholder:text-white/50"
               placeholder="⌘/ Search"
             />
-          </div>
+          </div> */}
         </div>
       </section>
-      {!partners.length && !mentors.length && !auditors.length && (
+      {/* {!partners.length && (
         <div className="container py-12 mx-auto text-center text-base leading-6 font-medium text-white/60 mb-3">
           {`No result for "${searchInput.trim()}"`}
         </div>
-      )}
-      {!!mentors.length && (
-        <Fade>
-          <section className="px-5 container mx-auto mb-12 md:mb-20">
-            <h2 className="mt-4 mb-6 md:mt-6 md:mb-10 text-[26px] leading-[35px] md:text-[32px] md:leading-[43px] text-white font-semibold">
-              Mentors of DefiEdge
-            </h2>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-4 md:gap-6 md:mb-6">
-              {mentors.map((e) => (
-                <Fade key={e.name}>
-                  <div className="ecosystem-card p-8">
-                    <div className="h-[60px] w-[60px] relative mb-[18px]">
+      )} */}
+      {/* {!!partners.length && ( */}
+      <Fade>
+        <section className="px-5 container mx-auto mb-12 md:mb-[120px] ">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-4 md:gap-6 md:mb-6">
+            {allPartners.map((e) => (
+              <Fade key={e.name}>
+                <div className="bg-ecosystem-card p-5 flex space-x-3">
+                  <div
+                    className={(
+                      "h-[40px] w-[40px] relative overflow-hidden" +
+                      " " +
+                      (e.logo
+                        ? ""
+                        : "flex items-center justify-center bg-white/10 !rounded-md") +
+                      " " +
+                      (e.rounded ? "rounded-full" : "rounded-sm")
+                    ).trim()}
+                    style={{ backgroundColor: e.background }}
+                  >
+                    {e.logo ? (
                       <Image src={e.logo} objectFit="contain" layout="fill" />
-                    </div>
-                    <h4 className="text-xl leading-7 md:text-2xl font-semibold text-white mb-3">
+                    ) : (
+                      <span className="text-base leading-5 tracking-wider font-medium text-white">
+                        {e.name
+                          .split(" ")
+                          .map((e) => e[0])
+                          .join("")}
+                      </span>
+                    )}
+                  </div>
+                  <div className="flex-1 ">
+                    <h4 className="text-base leading-5 font-semibold text-white mb-1">
                       {e.name}
                     </h4>
-                    <h4 className="text-base leading-6 font-medium text-white/60">
+                    <h4 className="text-sm leading-4 font-medium text-white/60">
                       {e.desc}
                     </h4>
                   </div>
-                </Fade>
-              ))}
-            </div>
-            {INITIAL_ITEMS_TO_SHOW < allMentors.length && !searchText && (
-              <div className="flex justify-center">
-                <LoadMoreButton
-                  active={showAllMentors}
-                  onClick={() => dispatch({ showAllMentors: !showAllMentors })}
-                />
-              </div>
-            )}
-          </section>
-        </Fade>
-      )}
-      {!!auditors.length && (
-        <Fade>
-          <section className="px-5 container mx-auto mb-12 md:mb-20">
-            <h2 className="mt-4 mb-6 md:mt-6 md:mb-10 text-[26px] leading-[35px] md:text-[32px] md:leading-[43px] text-white font-semibold">
-              Auditors
-            </h2>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-4 md:gap-6 md:mb-6">
-              {auditors.map((e) => (
-                <Fade key={e.name}>
-                  <div className="ecosystem-card p-8">
-                    <div className="h-[60px] w-[60px] relative mb-[18px]">
-                      <Image src={e.logo} objectFit="contain" layout="fill" />
-                    </div>
-                    <h4 className="text-xl leading-7 md:text-2xl font-semibold text-white mb-3">
-                      {e.name}
-                    </h4>
-                    <h4 className="text-base leading-6 font-medium text-white/60">
-                      {e.desc}
-                    </h4>
-                  </div>
-                </Fade>
-              ))}
-            </div>
-            {INITIAL_ITEMS_TO_SHOW < allAuditors.length && !searchText && (
-              <div className="flex justify-center">
-                <LoadMoreButton
-                  active={showAllAuditors}
-                  onClick={() =>
-                    dispatch({ showAllAuditors: !showAllAuditors })
-                  }
-                />
-              </div>
-            )}
-          </section>
-        </Fade>
-      )}
-      {!!partners.length && (
-        <Fade>
-          <section className="px-5 container mx-auto mb-12 md:mb-20">
-            <h2 className="mt-4 mb-6 md:mt-6 md:mb-10 text-[26px] leading-[35px] md:text-[32px] md:leading-[43px] text-white font-semibold">
-              Ecosystem Partners
-            </h2>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-4 md:gap-6 md:mb-6">
-              {partners.map((e) => (
-                <Fade key={e.name}>
-                  <div className="ecosystem-card p-8">
-                    <div className="h-[60px] w-[60px] relative mb-[18px]">
-                      <Image src={e.logo} objectFit="contain" layout="fill" />
-                    </div>
-                    <h4 className="text-xl leading-7 md:text-2xl font-semibold text-white mb-3">
-                      {e.name}
-                    </h4>
-                    <h4 className="text-base leading-6 font-medium text-white/60">
-                      {e.desc}
-                    </h4>
-                  </div>
-                </Fade>
-              ))}
-            </div>
-            {INITIAL_ITEMS_TO_SHOW < allPartners.length && !searchText && (
+                </div>
+              </Fade>
+            ))}
+          </div>
+          {/* {INITIAL_ITEMS_TO_SHOW < allPartners.length && !searchText && (
               <div className="flex justify-center">
                 <LoadMoreButton
                   active={showAllPartners}
@@ -254,10 +178,10 @@ export default function EcoSystemPage() {
                   }
                 />
               </div>
-            )}
-          </section>
-        </Fade>
-      )}
+            )} */}
+        </section>
+      </Fade>
+      {/* )} */}
       <div className="absolute w-screen top-0 h-full inset-x-0 z-0 pointer-events-none opacity-60">
         <Image
           src={Circle.src}
