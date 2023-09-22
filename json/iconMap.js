8782329b77f78dd90ca70e3001133993dfc1f1e5
wrongchain <wrongchain@unbound.finance>
2023-09-22 12:38:28 +0530

    ft: added icon map

---
+++ json/iconMap.js
@@ -0,0 +1,26 @@
+const iconMap = {
+  0: 'https://s2.coinmarketcap.com/static/img/coins/128x128/1027.png', // ethereum
+  1: 'https://s2.coinmarketcap.com/static/img/coins/128x128/3890.png', // polygon
+  2: 'https://s2.coinmarketcap.com/static/img/coins/128x128/11841.png', // arbitrum
+  3: 'https://s2.coinmarketcap.com/static/img/coins/128x128/11840.png', // optimism
+  4: 'https://s2.coinmarketcap.com/static/img/coins/128x128/1839.png', // BNB
+  5: 'https://s2.coinmarketcap.com/static/img/coins/128x128/27716.png', // base
+  6: 'https://s2.coinmarketcap.com/static/img/coins/128x128/22949.png', // camelot
+  7: 'https://s2.coinmarketcap.com/static/img/coins/128x128/23335.png', // thena
+  8: 'https://s2.coinmarketcap.com/static/img/coins/128x128/7186.png', // pancakeswap
+  9: 'https://s2.coinmarketcap.com/static/img/coins/128x128/6758.png', // sushiswap
+  10: 'https://s2.coinmarketcap.com/static/img/exchanges/128x128/7516.png', // retro
+  11: 'https://s2.coinmarketcap.com/static/img/coins/128x128/3179.png', // arbidex
+  12: 'https://s2.coinmarketcap.com/static/img/exchanges/128x128/1281.png', // apeswap
+  13: 'https://s2.coinmarketcap.com/static/img/coins/128x128/1975.png', // chainlink
+  14: '/images/ecosystem/merkl.png', // merkl
+  15: 'https://s2.coinmarketcap.com/static/img/coins/128x128/23836.png', // Équilibre
+  16: 'https://s2.coinmarketcap.com/static/img/exchanges/64x64/1553.png', // algebra
+  17: '', // cruise
+  18: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21846.png', // space id
+  19: 'https://overnight.fi/img/logo.01034063.svg', // overnight finance
+  20: 'https://pbs.twimg.com/profile_images/1377743719089254405/LQwS_x0l_400x400.jpg', // defillama
+  21: 'https://pbs.twimg.com/profile_images/1681396816737181707/MZVvpTPr_400x400.jpg', // zapper
+};
+
+export default iconMap;
