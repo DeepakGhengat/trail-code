e1c50423a1e1c3d53ceb167e8029d21190800b73
wrongchain <wrongchain@unbound.finance>
2023-11-28 09:45:22 +0530

    chore: updated iconmap

--- json/iconMap.js
+++ json/iconMap.js
@@ -16,11 +16,26 @@ const iconMap = {
   14: '/images/ecosystem/merkl.png', // merkl
   15: 'https://s2.coinmarketcap.com/static/img/coins/128x128/23836.png', // Équilibre
   16: 'https://s2.coinmarketcap.com/static/img/exchanges/64x64/1553.png', // algebra
-  17: '', // cruise
+  17: 'https://pbs.twimg.com/profile_images/1592782887006269440/_KjiKgiG_400x400.jpg', // cruise
   18: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21846.png', // space id
-  19: 'https://overnight.fi/img/logo.01034063.svg', // overnight finance
+  19: 'https://pbs.twimg.com/profile_images/1727301574181912576/EFg42goO_400x400.jpg', // overnight finance
   20: 'https://pbs.twimg.com/profile_images/1377743719089254405/LQwS_x0l_400x400.jpg', // defillama
   21: 'https://pbs.twimg.com/profile_images/1681396816737181707/MZVvpTPr_400x400.jpg', // zapper
+  22: 'https://1inch.io/assets/token-logo/1inch_token.png', // 1Inch
+  23: 'https://pbs.twimg.com/profile_images/1696986796478091264/79NZgGom_400x400.jpg', // Uniswap
+  24: 'https://pbs.twimg.com/profile_images/1645802735164608515/vOPYwvDf_400x400.jpg', // Kujira
+  25: 'https://pbs.twimg.com/profile_images/1715356180707442688/cb0L7h_U_400x400.jpg', // Horiza
+  26: 'https://pbs.twimg.com/profile_images/1660041171736080384/8Tt13tu7_400x400.png', // Ramses
+  27: 'https://pbs.twimg.com/profile_images/1702394800043868161/EGsd-c2z_400x400.jpg', // baseswap
+  28: 'https://pbs.twimg.com/profile_images/1526851278453751810/Tf2egz-j_400x400.jpg', // unbound finance
+  29: 'https://pbs.twimg.com/profile_images/1668560071490502656/HMr5khOI_400x400.jpg', // quickswap
+  30: 'https://pbs.twimg.com/profile_images/1607809611834298369/f-MClvFk_400x400.png', // pangolin
+  31: 'https://pbs.twimg.com/profile_images/1605605053901021184/9LNylZAA_400x400.png', // avalanche
+  32: 'https://pbs.twimg.com/profile_images/1647563917349060608/S1P_lNAN_400x400.jpg', // zkSync
+  33: 'https://pbs.twimg.com/profile_images/1618226686511747072/s_X7BLTk_400x400.png', // nexus mutual
+  34: 'https://pbs.twimg.com/profile_images/1630924828592033796/_48-NsTv_400x400.png', // opencover
+  35: 'https://pbs.twimg.com/profile_images/1572885646112219136/XpMRWwtg_400x400.jpg', // fjord foundry
+  36: 'https://pbs.twimg.com/profile_images/1626158370234441729/j8C3yuc2_400x400.jpg', // openpad
 };
 
 export default iconMap;
